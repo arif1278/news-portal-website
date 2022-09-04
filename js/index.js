f62783a2089dd84e7289f40b551db3d0048d0b1a
arifhossain <manabethreeplusone@gmail.com>
2022-09-04 08:20:22 +0600

    2nd commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 const allNews = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/news/categories")
     const data = await response.json();
-    return data;
+    // return data;
+    displayCategories(data.data.news_category);
 }
-const setAllMeno = async () => {
-    const data = await allNews();
-    for (const news_category of data) {
-        console.log(news_category)
-    }
+
+const displayCategories = (category) => {
+    const categoriesContainer = document.getElementById('categori');
+    category.forEach(item => {
+        const li = document.createElement('li');
+        li.classList.add('menu', 'menu-horizontal', 'bg-base-100', 'mt-7')
+        li.innerHTML = `
+        <li><a onclick="loadCatDetails(${item.category_id})" class="text-2xl"${item.category_name}</a></li>
+        `;
+        categoriesContainer.appendChild(li);
+    });
 }
-setAllMeno();
-// allNews();
+
+allNews();
